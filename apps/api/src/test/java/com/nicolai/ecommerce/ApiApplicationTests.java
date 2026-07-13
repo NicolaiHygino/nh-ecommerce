29ffe5a4ca4e169e87958e290188e8859ebbf376
Nicolai Hygino <nicolaihygino2000@gmail.com>
2026-07-12 21:09:44 -0300

    feat: setting up project api, web and docker

---
+++ apps/api/src/test/java/com/nicolai/ecommerce/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolai.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
